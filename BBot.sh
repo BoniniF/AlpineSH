113b5016169e36a381652ce0bc76e57559371fb2
BoniniF <filippomariabonini@libero.it>
2026-04-01 17:41:28 +0000

    f

--- BBot.sh
+++ BBot.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
-if [ -n "$1" ]; then
-prompt=$1
-else
-prompt="Hi"
-fi
 
-if [ -n "$2" ]; then
-model=$2
-else
-model="nvidia/nemotron-nano-12b-v2-vl"
-fi
+prompt="${1:-Hi}"
+
+model="${2:-nvidia/nemotron-nano-12b-v2-vl}"
 
 #echo "answer=\$(wget -q -O - --header=\'Content-Type: application/json\' --post-data=\'{\"model\": \"$model\", \"messages\": [{\"role\": \"user\", \"content\": \"$prompt\"}]}\' https://watchllm.vercel.app/api/proxy)"
 
-answer=$(wget -q -O - --header="Content-Type: application/json" --post-data="{\"model\": \"$model\", \"messages\": [{\"role\": \"user\", \"content\": \"$prompt\"}]}" https://watchllm.vercel.app/api/proxy)
+answer=$(wget -q -O - --header="Content-Type: application/json" --post-data="{\"model\": \"$model\", \"messages\": [{\"role\": \"user\", \"content\": \"$prompt\"}]}" https://watchllm.vercel.app/api/proxy )
+#echo " wget -q -O - --header=\"Content-Type: application/json\" --post-data=\"{\"model\": \"$model\", \"messages\": [{\"role\": \"user\", \"content\": \"$prompt\"}]}\" https://watchllm.vercel.app/api/proxy "
 
-echo $answer
+echo "$answer"
 echo ""
 
-# Estrae il contenuto del messaggio, interpreta i \n e poi isola il blocco sh
-run=$(echo "$answer" | jq -r '.choices[0].message.content' | sed -n '/^```sh/,/^```/ { /^```/d; p; }')
-run
+
+run=$(echo "$answer" | sed -n '/^```\(sh\|bash\)/,/^```/ { /^```/d; p; }')
+echo "$run" | sh
